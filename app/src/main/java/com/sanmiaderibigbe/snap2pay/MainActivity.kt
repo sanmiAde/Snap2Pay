9eb34d62bc3b909fc718a4ac1a8dbe9c630a3f08
Oluwasanmi Aderibigbe <sanmiaderibigbe@outlook.com>
2019-06-30 00:10:30 +0100

    first commit

---
+++ app/src/main/java/com/sanmiaderibigbe/snap2pay/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sanmiaderibigbe.snap2pay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
